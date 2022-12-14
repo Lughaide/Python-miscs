104b50e87e8c89c313bf702aa8e7dff6171eb354
Lughaide <19520504@gm.uit.edu.vn>
2022-12-14 23:08:04 +0700

    aaa

--- NLP/test.py
+++ NLP/test.py
@@ -1,4 +1,9 @@
 import numpy as np
 
-print("HEY")
-print(np.random.rand(1, 2))
+with open("practice_2.txt", "r", encoding='utf-8') as f:
+    print(f.name)
+    print("-"*100)
+    for line in f:
+        for lett in line:
+            print(lett.lower(), end="")
+    print("-"*100)
